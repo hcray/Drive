571cd2184cdf45c33cd53aa22a37921660134671
caiyingying <caiyingying234@gmail.com>
2015-01-12 19:53:05 +0800

    路线activity 中语音的添加

--- src/com/daoliuhe/drive/LineActivity.java
+++ src/com/daoliuhe/drive/LineActivity.java
@@ -1,17 +1,234 @@
 package com.daoliuhe.drive;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class LineActivity extends Activity {
 	public static final String SETTING_INFOS = "LineActivity";
 	//private static final int ACTIVITY_LOGIN = 0;
+	//语音
+	private TextToSpeech ttSpeech;
 
+	private Button btnLights1;
+	private Button btnLights2;
+	private Button btnLights3;
+	private Button btnLights4;
+	private Button btnTurnRight;
+	
+	private Button btnSidewalk;
+	private Button btnPassSidewalk;
+	private Button btnTurnLeft;
+	private Button btnAheadDirectLine;
+	private Button btnPassBusStation;
+	
+	private Button btnDirectLine;
+	private Button btnPassSchool;
+	private Button btnChangeLanes;
+	private Button btnSlowdown;
+	private Button btnSpeedLimit;
+	
+	private Button btnPassSchoolStation;
+	private Button btnTurn;
+	private Button btnPullOver;
+	private Button btnBackCar;
+	private Button btnReset;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_line);
+
+		ttSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+			
+			@Override
+			public void onInit(int status) {
+			     // TODO Auto-generated method stub
+			     if(status == TextToSpeech.SUCCESS){
+			         //设置朗读语言
+			         int supported = ttSpeech.setLanguage(Locale.CHINA);
+			         if((supported != TextToSpeech.LANG_AVAILABLE)&&(supported != TextToSpeech.LANG_COUNTRY_AVAILABLE)){
+			             //displayToast("不支持当前语言！");
+			         }
+			         // 设置音调，值越大声音越尖（女生），值越小则变成男声,1.0是常规  
+			         ttSpeech.setPitch(0.5f);
+			     }
+			 }
+		});
+		
+		btnLights1 = (Button) this.findViewById(R.id.btnLights1);
+		btnLights1.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				ttSpeech.speak("模拟灯光一", TextToSpeech.QUEUE_FLUSH, null);
+			}
+			
+		});
+		btnLights2 = (Button) this.findViewById(R.id.btnLights2);
+		btnLights2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		btnLights3 = (Button) this.findViewById(R.id.btnLights3);
+		btnLights3.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnLights4 = (Button) this.findViewById(R.id.btnLights4);
+		btnLights4.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnTurnRight = (Button) this.findViewById(R.id.btnTurnRight);
+		btnTurnRight.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		
+		btnSidewalk = (Button) this.findViewById(R.id.btnSidewalk);
+		btnSidewalk.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		btnPassSidewalk = (Button) this.findViewById(R.id.btnPassSidewalk);
+		btnPassSidewalk.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnTurnLeft = (Button) this.findViewById(R.id.btnTurnLeft);
+		btnTurnLeft.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnAheadDirectLine = (Button) this.findViewById(R.id.btnAheadDirectLine);
+		btnAheadDirectLine.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnPassBusStation = (Button) this.findViewById(R.id.btnPassBusStation);
+		btnPassBusStation.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+
+		btnDirectLine = (Button) this.findViewById(R.id.btnDirectLine);
+		btnDirectLine.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnPassSchool = (Button) this.findViewById(R.id.btnPassSchool);
+		btnPassSchool.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnChangeLanes = (Button) this.findViewById(R.id.btnChangeLanes);
+		btnChangeLanes.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnSlowdown = (Button) this.findViewById(R.id.btnSlowdown);
+		btnSlowdown.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnSpeedLimit = (Button) this.findViewById(R.id.btnSpeedLimit);
+		btnSpeedLimit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+
+		btnPassSchoolStation = (Button) this.findViewById(R.id.btnPassSchoolStation);
+		btnPassSchoolStation.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnTurn = (Button) this.findViewById(R.id.btnTurn);
+		btnTurn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnPullOver = (Button) this.findViewById(R.id.btnPullOver);
+		btnPullOver.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnBackCar = (Button) this.findViewById(R.id.btnBackCar);
+		btnBackCar.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		btnReset = (Button) this.findViewById(R.id.btnReset);
+		btnReset.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}});
+		
 	}
 
 	@Override
@@ -19,6 +236,14 @@ public class LineActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.line, menu);
 		return true;
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if(ttSpeech != null){
+			ttSpeech.shutdown();//关闭TTS
+		}
 	}
 
 }
