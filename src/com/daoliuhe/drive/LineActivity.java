c64f123d1d948d429c0c6b12e51b9c3f352c8ce7
caiyingying <caiyingying234@gmail.com>
2015-01-13 20:00:16 +0800

    播放声音的注释，长按设置坐标的添加

--- src/com/daoliuhe/drive/LineActivity.java
+++ src/com/daoliuhe/drive/LineActivity.java
@@ -1,87 +1,147 @@
 package com.daoliuhe.drive;
 
+import java.util.Iterator;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.location.Criteria;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LineActivity extends Activity {
+public class LineActivity extends Activity implements OnLongClickListener{
+
 	public static final String SETTING_INFOS = "LineActivity";
-	//private static final int ACTIVITY_LOGIN = 0;
-	//语音
+	// private static final int ACTIVITY_LOGIN = 0;
+	private static final String TAG = "LineActivity";
+	// 语音
 	private SoundPool soundPool;
+	// GPS定位
+	private LocationManager lm;
+
+	private TextView tvline;
 
 	private Button btnLights1;
 	private Button btnLights2;
 	private Button btnLights3;
 	private Button btnLights4;
 	private Button btnTurnRight;
-	
+
 	private Button btnSidewalk;
 	private Button btnPassSidewalk;
 	private Button btnTurnLeft;
 	private Button btnAheadDirectLine;
 	private Button btnPassBusStation;
-	
+
 	private Button btnDirectLine;
 	private Button btnPassSchool;
 	private Button btnChangeLanes;
 	private Button btnSlowdown;
 	private Button btnSpeedLimit;
-	
+
 	private Button btnPassSchoolStation;
 	private Button btnTurn;
 	private Button btnPullOver;
 	private Button btnBackCar;
 	private Button btnReset;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_line);
 
-		soundPool= new SoundPool(10,AudioManager.STREAM_SYSTEM,5);
-		soundPool.load(this,R.raw.snd01,1);
-		soundPool.load(this,R.raw.snd02,1);
-		soundPool.load(this,R.raw.snd03,1);
-		soundPool.load(this,R.raw.snd04,1);
-		soundPool.load(this,R.raw.snd05,1);
-		soundPool.load(this,R.raw.snd06,1);
-		soundPool.load(this,R.raw.snd07,1);
-		soundPool.load(this,R.raw.snd08,1);
-		soundPool.load(this,R.raw.snd09,1);
-		soundPool.load(this,R.raw.snd10,1);
-		soundPool.load(this,R.raw.snd11,1);
-		soundPool.load(this,R.raw.snd12,1);
-		soundPool.load(this,R.raw.snd13,1);
-		soundPool.load(this,R.raw.snd14,1);
-		soundPool.load(this,R.raw.snd15,1);
-		soundPool.load(this,R.raw.snd16,1);
-		soundPool.load(this,R.raw.snd17,1);
-		soundPool.load(this,R.raw.snd18,1);
-		soundPool.load(this,R.raw.snd19,1);
+		tvline = (TextView) this.findViewById(R.id.tv_line);
+
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		// 判断GPS是否正常启动
+		if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+			Toast.makeText(this, "请开启GPS导航...", Toast.LENGTH_SHORT).show();
+			// 返回开启GPS导航设置界面
+			// Intent intent = new
+			// Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+			// startActivityForResult(intent,0);
+			// return;
+		}
+
+		// 为获取地理位置信息时设置查询条件
+		String bestProvider = lm.getBestProvider(getCriteria(), true);
+		// 获取位置信息
+		// 如果不设置查询要求，getLastKnownLocation方法传人的参数为LocationManager.GPS_PROVIDER
+		Location location = lm.getLastKnownLocation(bestProvider);
+		updateView(location);
+		// 监听状态
+		lm.addGpsStatusListener(listener);
+		// 绑定监听，有4个参数
+		// 参数1，设备：有GPS_PROVIDER和NETWORK_PROVIDER两种
+		// 参数2，位置信息更新周期，单位毫秒
+		// 参数3，位置变化最小距离：当位置距离变化超过此值时，将更新位置信息
+		// 参数4，监听
+		// 备注：参数2和3，如果参数3不为0，则以参数3为准；参数3为0，则通过时间来定时更新；两者为0，则随时刷新
+
+		// 1秒更新一次，或最小位移变化超过1米更新一次；
+		// 注意：此处更新准确度非常低，推荐在service里面启动一个Thread，在run中sleep(10000);然后执行handler.sendMessage(),更新位置
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1,
+				locationListener);
+
+		soundPool = new SoundPool(19, AudioManager.STREAM_SYSTEM, 5);
+		soundPool.load(this, R.raw.snd01, 1);
+		/*
+		soundPool.load(this, R.raw.snd02, 1);
+		soundPool.load(this, R.raw.snd03, 1);
+		soundPool.load(this, R.raw.snd04, 1);
+		soundPool.load(this, R.raw.snd05, 1);
+		soundPool.load(this, R.raw.snd06, 1);
+		soundPool.load(this, R.raw.snd07, 1);
+		soundPool.load(this, R.raw.snd08, 1);
+		soundPool.load(this, R.raw.snd09, 1);
+		soundPool.load(this, R.raw.snd10, 1);
+		soundPool.load(this, R.raw.snd11, 1);
+		soundPool.load(this, R.raw.snd12, 1);
+		soundPool.load(this, R.raw.snd13, 1);
+		soundPool.load(this, R.raw.snd14, 1);
+		soundPool.load(this, R.raw.snd15, 1);
+		soundPool.load(this, R.raw.snd16, 1);
+		soundPool.load(this, R.raw.snd17, 1);
+		soundPool.load(this, R.raw.snd18, 1);
+		soundPool.load(this, R.raw.snd19, 1);
+		*/
 		
 		btnLights1 = (Button) this.findViewById(R.id.btnLights1);
 		btnLights1.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				//id,左右声道, 音量, 优先级, 是否循环(0为不循环，-1为循环),播放比率(从0.5到2，一般为1，表示正常播放)
+				// id,左右声道, 音量, 优先级, 是否循环(0为不循环，-1为循环),播放比率(从0.5到2，一般为1，表示正常播放)
 				soundPool.play(1, 1, 1, 0, 0, 1);
 			}
-			
 		});
+		btnLights1.setOnLongClickListener(this);
+		
 		btnLights2 = (Button) this.findViewById(R.id.btnLights2);
 		btnLights2.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(2, 1, 1, 0, 0, 1);
-				
+
 			}
 		});
 		btnLights3 = (Button) this.findViewById(R.id.btnLights3);
@@ -90,32 +150,35 @@ public class LineActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				soundPool.play(3, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnLights4 = (Button) this.findViewById(R.id.btnLights4);
 		btnLights4.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				soundPool.play(4, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnTurnRight = (Button) this.findViewById(R.id.btnTurnRight);
 		btnTurnRight.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				soundPool.play(5, 1, 1, 0, 0, 1);
-				
-			}});
-		
+
+			}
+		});
+
 		btnSidewalk = (Button) this.findViewById(R.id.btnSidewalk);
 		btnSidewalk.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(6, 1, 1, 0, 0, 1);
-				
+
 			}
 		});
 		btnPassSidewalk = (Button) this.findViewById(R.id.btnPassSidewalk);
@@ -124,117 +187,296 @@ public class LineActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				soundPool.play(7, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnTurnLeft = (Button) this.findViewById(R.id.btnTurnLeft);
 		btnTurnLeft.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(8, 1, 1, 0, 0, 1);
-				
-			}});
-		btnAheadDirectLine = (Button) this.findViewById(R.id.btnAheadDirectLine);
+
+			}
+		});
+		btnAheadDirectLine = (Button) this
+				.findViewById(R.id.btnAheadDirectLine);
 		btnAheadDirectLine.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(9, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnPassBusStation = (Button) this.findViewById(R.id.btnPassBusStation);
 		btnPassBusStation.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(10, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 
 		btnDirectLine = (Button) this.findViewById(R.id.btnDirectLine);
 		btnDirectLine.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(11, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnPassSchool = (Button) this.findViewById(R.id.btnPassSchool);
 		btnPassSchool.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(12, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnChangeLanes = (Button) this.findViewById(R.id.btnChangeLanes);
 		btnChangeLanes.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(13, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnSlowdown = (Button) this.findViewById(R.id.btnSlowdown);
 		btnSlowdown.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(14, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnSpeedLimit = (Button) this.findViewById(R.id.btnSpeedLimit);
 		btnSpeedLimit.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(15, 1, 1, 0, 0, 1);
-				
-			}});
 
-		btnPassSchoolStation = (Button) this.findViewById(R.id.btnPassSchoolStation);
+			}
+		});
+
+		btnPassSchoolStation = (Button) this
+				.findViewById(R.id.btnPassSchoolStation);
 		btnPassSchoolStation.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(16, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnTurn = (Button) this.findViewById(R.id.btnTurn);
 		btnTurn.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(17, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnPullOver = (Button) this.findViewById(R.id.btnPullOver);
 		btnPullOver.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(18, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnBackCar = (Button) this.findViewById(R.id.btnBackCar);
 		btnBackCar.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				soundPool.play(19, 1, 1, 0, 0, 1);
-				
-			}});
+
+			}
+		});
 		btnReset = (Button) this.findViewById(R.id.btnReset);
 		btnReset.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
-			}});
-		
+
+			}
+		});
+
+	}
+	
+	@Override
+	public boolean onLongClick(View v) {
+		showDialog(1);
+		Log.i(TAG, "view: " + v.getId());
+		return true;
+	}
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		return buildDialog(LineActivity.this);
 	}
 
+	// 位置监听
+	private LocationListener locationListener = new LocationListener() {
+
+		/**
+		 * 位置信息变化时触发
+		 */
+		public void onLocationChanged(Location location) {
+			updateView(location);
+			Log.i(TAG, "时间：" + location.getTime());
+			Log.i(TAG, "经度：" + location.getLongitude());
+			Log.i(TAG, "纬度：" + location.getLatitude());
+			Log.i(TAG, "海拔：" + location.getAltitude());
+		}
+
+		/**
+		 * GPS状态变化时触发
+		 */
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			switch (status) {
+			// GPS状态为可见时
+			case LocationProvider.AVAILABLE:
+				Log.i(TAG, "当前GPS状态为可见状态");
+				break;
+			// GPS状态为服务区外时
+			case LocationProvider.OUT_OF_SERVICE:
+				Log.i(TAG, "当前GPS状态为服务区外状态");
+				break;
+			// GPS状态为暂停服务时
+			case LocationProvider.TEMPORARILY_UNAVAILABLE:
+				Log.i(TAG, "当前GPS状态为暂停服务状态");
+				break;
+			}
+		}
+
+		/**
+		 * GPS开启时触发
+		 */
+		public void onProviderEnabled(String provider) {
+			Location location = lm.getLastKnownLocation(provider);
+			updateView(location);
+		}
+
+		/**
+		 * GPS禁用时触发
+		 */
+		public void onProviderDisabled(String provider) {
+			updateView(null);
+		}
+
+	};
+
+	// 状态监听
+	GpsStatus.Listener listener = new GpsStatus.Listener() {
+		public void onGpsStatusChanged(int event) {
+			switch (event) {
+			// 第一次定位
+			case GpsStatus.GPS_EVENT_FIRST_FIX:
+				Log.i(TAG, "第一次定位");
+				break;
+			// 卫星状态改变
+			case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
+				Log.i(TAG, "卫星状态改变");
+				// 获取当前状态
+				GpsStatus gpsStatus = lm.getGpsStatus(null);
+				// 获取卫星颗数的默认最大值
+				int maxSatellites = gpsStatus.getMaxSatellites();
+				// 创建一个迭代器保存所有卫星
+				Iterator<GpsSatellite> iters = gpsStatus.getSatellites()
+						.iterator();
+				int count = 0;
+				while (iters.hasNext() && count <= maxSatellites) {
+					GpsSatellite s = iters.next();
+					count++;
+				}
+				System.out.println("搜索到：" + count + "颗卫星");
+				break;
+			// 定位启动
+			case GpsStatus.GPS_EVENT_STARTED:
+				Log.i(TAG, "定位启动");
+				break;
+			// 定位结束
+			case GpsStatus.GPS_EVENT_STOPPED:
+				Log.i(TAG, "定位结束");
+				break;
+			}
+		};
+	};
+
+	/**
+	 * 实时更新文本内容
+	 * 
+	 * @param location
+	 */
+	private void updateView(Location location) {
+		if (location != null) {
+			tvline.setText("设备位置信息\n\n经度：");
+			tvline.append(String.valueOf(location.getLongitude()));
+			tvline.append("\n纬度：");
+			tvline.append(String.valueOf(location.getLatitude()));
+		} else {
+			// 清空EditText对象
+			tvline.setText("");
+		}
+	}
+
+	/**
+	 * 返回查询条件
+	 * 
+	 * @return
+	 */
+	private Criteria getCriteria() {
+		Criteria criteria = new Criteria();
+		// 设置定位精确度 Criteria.ACCURACY_COARSE比较粗略，Criteria.ACCURACY_FINE则比较精细
+		criteria.setAccuracy(Criteria.ACCURACY_FINE);
+		// 设置是否要求速度
+		criteria.setSpeedRequired(false);
+		// 设置是否允许运营商收费
+		criteria.setCostAllowed(false);
+		// 设置是否需要方位信息
+		criteria.setBearingRequired(false);
+		// 设置是否需要海拔信息
+		criteria.setAltitudeRequired(false);
+		// 设置对电源的需求
+		criteria.setPowerRequirement(Criteria.POWER_LOW);
+		return criteria;
+	}
+	
+	
+	private Dialog buildDialog(Context context) {
+		LayoutInflater inflater = LayoutInflater.from(this);
+		final View textEntryView = inflater.inflate(
+				R.layout.alert_dialog_text_entry, null);
+		AlertDialog.Builder builder = new AlertDialog.Builder(context);
+		//builder.setIcon(R.drawable.alert_dialog_icon);
+		builder.setTitle(R.string.alert_dialog_text_entry);
+		builder.setView(textEntryView);
+		builder.setPositiveButton(R.string.alert_dialog_ok,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						setTitle("点击了对话框上的确定按钮");
+					}
+				});
+		builder.setNegativeButton(R.string.alert_dialog_cancel,
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+						setTitle("点击了对话框上的取消按钮");
+					}
+				});
+		return builder.create();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -245,6 +487,10 @@ public class LineActivity extends Activity {
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
+		soundPool.release();
+		lm.removeUpdates(locationListener);
 	}
+
+
 
 }
