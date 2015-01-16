6c2e6aed2bdda7ffce46b9181c26a61530ec109a
caiyingying <caiyingying234@gmail.com>
2015-01-16 17:40:15 +0800

    参数传递

--- src/com/daoliuhe/drive/SubjectActivity.java
+++ src/com/daoliuhe/drive/SubjectActivity.java
@@ -42,7 +42,7 @@ public class SubjectActivity extends Activity {
 	
 	private DbAdapter dbAdapter;
 	
-	private RelativeLayout  layout;
+	//private RelativeLayout  layout;
 	
 	private List<LineBean> lineList;
 
@@ -51,7 +51,7 @@ public class SubjectActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_subject);
 		
-		layout = (RelativeLayout ) this.findViewById(R.id.subject_relativeLayout);
+		//layout = (RelativeLayout ) this.findViewById(R.id.subject_relativeLayout);
 		//数据库
 		dbAdapter = new DbAdapter(this);
 		dbAdapter.open();
@@ -87,15 +87,8 @@ public class SubjectActivity extends Activity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		if(requestCode == ACTIVITY_ADD && resultCode == RESULT_OK){
-			int retId = data.getIntExtra("id", -1);
-			String retName = data.getStringExtra("name");
-			if(retId != -1){
-				Button button = new Button(this);
-				button.setId(retId);
-				button.setText(retName);
-				//将按钮放入layout组件
-				layout.addView(button);   
-			}
+			//刷新路线列表
+			renderListView();
 		}
 	}
 	
@@ -110,9 +103,134 @@ public class SubjectActivity extends Activity {
 			public void onItemClick(AdapterView<?> arg0, View arg1, int position,
 					long arg3) {
 				int id = lineList.get(position).getId();
+				String lineName = lineList.get(position).getLineName();
+				
+				Double turnRightLat = lineList.get(position).getTurnRightLat();
+				Double turnRightLng = lineList.get(position).getTurnRightLng();
+				Double sidewalkLat = lineList.get(position).getSidewalkLat();
+				Double sidewalkLng = lineList.get(position).getSidewalkLng();
+				Double passSidewalkLat = lineList.get(position).getPassSidewalkLat();
+				Double passSidewalkLng = lineList.get(position).getPassSidewalkLng();
+				Double turnLeftLat = lineList.get(position).getTurnLat();
+				Double turnLeftLng = lineList.get(position).getTurnLng();
+				Double aheadDirectLineLat = lineList.get(position).getAheadDirectLineLat();
+				Double aheadDirectLineLng = lineList.get(position).getAheadDirectLineLng();
+				Double passBusStationLat = lineList.get(position).getPassBusStationLat();
+				Double passBusStationLng = lineList.get(position).getPassBusStationLng();
+				Double directLineLat = lineList.get(position).getDirectLineLat();
+				Double directLineLng = lineList.get(position).getDirectLineLng();
+				Double passSchoolLat = lineList.get(position).getPassSchoolLat();
+				Double passSchoolLng = lineList.get(position).getPassSchoolLng();
+				Double changeLanesLat = lineList.get(position).getChangeLanesLat();
+				Double changeLanesLng = lineList.get(position).getChangeLanesLng();
+				Double slowdownLat = lineList.get(position).getSlowdownLat();
+				Double slowdownLng = lineList.get(position).getSlowdownLng();
+				Double speedLimitLat = lineList.get(position).getSpeedLimitLat();
+				Double speedLimitLng = lineList.get(position).getSpeedLimitLng();
+				Double passSchoolStationLat = lineList.get(position).getPassSchoolStationLat();
+				Double passSchoolStationLng = lineList.get(position).getPassSchoolStationLng();
+				Double turnLat = lineList.get(position).getTurnLat();
+				Double turnLng = lineList.get(position).getTurnLng();
+				Double pullOverLat = lineList.get(position).getPullOverLat();
+				Double pullOverLng = lineList.get(position).getPullOverLng();
+				Double backCarLat = lineList.get(position).getBackCarLat();
+				Double backCarLng = lineList.get(position).getBackCarLng();
+				
 				Intent intent = new Intent();
 				intent.setClass(SubjectActivity.this, LineActivity.class);
 				intent.putExtra(DbAdapter.KEY_ID, id);
+				intent.putExtra(DbAdapter.KEY_LINE_NAME, lineName);
+				if(null != turnRightLat){
+					intent.putExtra(DbAdapter.KEY_TURNRIGHT_LAT, turnRightLat);
+				}
+				
+				if(null != turnRightLng){
+				}
+				
+				if(null != sidewalkLat){
+				}
+				
+				if(null != sidewalkLng){
+				}
+				
+				if(null != passSidewalkLat){
+				}
+				
+				if(null != passSidewalkLng){
+				}
+				
+				if(null != turnLeftLat){
+				}
+				
+				if(null != turnLeftLng){
+				}
+				
+				if(null != aheadDirectLineLat){
+				}
+				
+				if(null != aheadDirectLineLng){
+				}
+				
+				if(null != passBusStationLat){
+				}
+				
+				if(null != passBusStationLng){
+				}
+				
+				if(null != directLineLat){
+				}
+				
+				if(null != directLineLng){
+				}
+				
+				if(null != passSchoolLat){
+				}
+				
+				if(null != passSchoolLng){
+				}
+				
+				if(null != changeLanesLat){
+				}
+				
+				if(null != changeLanesLng){
+				}
+				
+				if(null != slowdownLat){
+				}
+				
+				if(null != slowdownLng){
+				}
+				
+				if(null != speedLimitLat){
+				}
+				
+				if(null != speedLimitLng){
+				}
+				
+				if(null != passSchoolStationLat){
+				}
+				
+				if(null != passSchoolStationLng){
+				}
+				
+				if(null != turnLat){
+				}
+				
+				if(null != turnLng){
+				}
+				
+				if(null != pullOverLat){
+				}
+				
+				if(null != pullOverLng){
+				}
+				
+				if(null != backCarLat){
+				}
+				
+				if(null != backCarLng){
+				}
+				
 				startActivityForResult(intent, ACTIVITY_ITEMVIEW);
 			}
         	
