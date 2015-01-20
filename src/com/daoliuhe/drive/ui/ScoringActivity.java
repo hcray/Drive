5dd51a2c99a42af5e6d97d794f487504dbdedc55
caiyingying <caiyingying234@gmail.com>
2015-01-20 22:04:26 +0800

    左右分栏的设置

--- src/com/daoliuhe/drive/ui/ScoringActivity.java
+++ src/com/daoliuhe/drive/ui/ScoringActivity.java
@@ -14,6 +14,8 @@ public class ScoringActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scoring);
+		
+		
 	}
 
 	@Override
