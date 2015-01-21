0feb963da32ec06fe0eb89e7a93aec3e548a239a
caiyingying <caiyingying234@gmail.com>
2015-01-21 22:50:14 +0800

    listbean的新增

--- src/com/daoliuhe/drive/ui/ScoringActivity.java
+++ src/com/daoliuhe/drive/ui/ScoringActivity.java
@@ -4,15 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
+import android.widget.ListView;
 
 import com.daoliuhe.drive.R;
 
 public class ScoringActivity extends Activity {
+	private ListView leftList;
+	
+	private ListView rightList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scoring);
+		
+		leftList = (ListView) this.findViewById(R.id.leftList);
+		
+		rightList = (ListView) this.findViewById(R.id.rightList);
+		
+		
 		//5√Î∫Ûπÿ±’
 		Handler handler = new Handler();  
         handler.postDelayed(new Runnable() {  
