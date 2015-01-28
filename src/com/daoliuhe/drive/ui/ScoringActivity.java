2bed8a63a392797628185d5bc5ce7ff9963eb471
caiyingying <caiyingying234@gmail.com>
2015-01-28 11:36:25 +0800

    左边点击，右边展示

--- src/com/daoliuhe/drive/ui/ScoringActivity.java
+++ src/com/daoliuhe/drive/ui/ScoringActivity.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import android.app.Activity;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -13,6 +12,8 @@ import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.daoliuhe.drive.R;
@@ -29,6 +30,8 @@ public class ScoringActivity extends Activity {
 	
 	private List<String> rightList = new ArrayList<String>();
 	
+	private ListViewActivityAdapter adapterRight;
+	
 	//关闭页面标志
 	private boolean closeFlag = true;
 	
@@ -37,7 +40,7 @@ public class ScoringActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_scoring);
 		
-		initList();
+		initList(0);
 		
 		OnTouchListener touchListener = new OnTouchListener(){
 			@Override
@@ -50,9 +53,62 @@ public class ScoringActivity extends Activity {
 			}
 		};
 		
+        OnItemClickListener itemClickListener =  new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View view, int position,
+					long arg3) {
+				Log.d(TAG, "onItemClick() position: " + position);
+				int viewId = 0;
+				switch(position){
+				case 0:
+					viewId = R.string.item1;
+					break;
+				case 1:
+					viewId = R.string.item2;
+					break;
+				case 2:
+					viewId = R.string.item3;
+					break;
+				case 3:
+					viewId = R.string.item4;
+					break;
+				case 4:
+					viewId = R.string.item5;
+					break;
+				case 5:
+					viewId = R.string.item6;
+					break;
+				case 6:
+					viewId = R.string.item7;
+					break;
+				case 7:
+					viewId = R.string.item8;
+					break;
+				case 8:
+					viewId = R.string.item9;
+					break;
+				case 9:
+					viewId = R.string.item10;
+					break;
+				case 10:
+					viewId = R.string.item11;
+					break;
+				case 11:
+					viewId = R.string.item12;
+					break;
+				case 12:
+					viewId = R.string.item13;
+					break;
+				}
+				clickLeftListItem(viewId);
+			}
+        };
+		
 		leftListView = (ListView) this.findViewById(R.id.leftList);
 		
 		leftListView.setOnTouchListener(touchListener);
+		
+		leftListView.setOnItemClickListener(itemClickListener);
 		
 		rightListView = (ListView) this.findViewById(R.id.rightList);
 		
@@ -61,7 +117,7 @@ public class ScoringActivity extends Activity {
 		ListViewActivityAdapter adapterLeft = new ListViewActivityAdapter(this, leftList);
 		leftListView.setAdapter(adapterLeft);
 	
-		ListViewActivityAdapter adapterRight = new ListViewActivityAdapter(this, rightList);
+		adapterRight = new ListViewActivityAdapter(this, rightList);
 		rightListView.setAdapter(adapterRight);
 		
 		//5秒后关闭
@@ -93,7 +149,12 @@ public class ScoringActivity extends Activity {
 		return super.onTouchEvent(event);
 	}
 
-	private void initList(){
+	
+	/**
+	 * 初始化list
+	 * @param id
+	 */
+	private void initList(int id){
 		
 		String item1Name = this.getResources().getString(R.string.item1);
 		String item2Name = this.getResources().getString(R.string.item2);
@@ -123,20 +184,19 @@ public class ScoringActivity extends Activity {
 		leftList.add(item12Name);
 		leftList.add(item13Name);
 		
-		Resources res =getResources();
-		String[] item1Detail = res.getStringArray(R.array.item1Detail);
-		String[] item2Detail = res.getStringArray(R.array.item2Detail);
-		String[] item3Detail = res.getStringArray(R.array.item3Detail);
-		String[] item4Detail = res.getStringArray(R.array.item4Detail);
-		String[] item5Detail = res.getStringArray(R.array.item5Detail);
-		String[] item6Detail = res.getStringArray(R.array.item6Detail);
-		String[] item7Detail = res.getStringArray(R.array.item7Detail);
-		String[] item8Detail = res.getStringArray(R.array.item8Detail);
-		String[] item9Detail = res.getStringArray(R.array.item9Detail);
-		String[] item10Detail = res.getStringArray(R.array.item10Detail);
-		String[] item11Detail = res.getStringArray(R.array.item11Detail);
-		String[] item12Detail = res.getStringArray(R.array.item12Detail);
-		String[] item13Detail = res.getStringArray(R.array.item13Detail);
+		String[] item1Detail = this.getResources().getStringArray(R.array.item1Detail);
+		String[] item2Detail = this.getResources().getStringArray(R.array.item2Detail);
+		String[] item3Detail = this.getResources().getStringArray(R.array.item3Detail);
+		String[] item4Detail = this.getResources().getStringArray(R.array.item4Detail);
+		String[] item5Detail = this.getResources().getStringArray(R.array.item5Detail);
+		String[] item6Detail = this.getResources().getStringArray(R.array.item6Detail);
+		String[] item7Detail = this.getResources().getStringArray(R.array.item7Detail);
+		String[] item8Detail = this.getResources().getStringArray(R.array.item8Detail);
+		String[] item9Detail = this.getResources().getStringArray(R.array.item9Detail);
+		String[] item10Detail = this.getResources().getStringArray(R.array.item10Detail);
+		String[] item11Detail = this.getResources().getStringArray(R.array.item11Detail);
+		String[] item12Detail = this.getResources().getStringArray(R.array.item12Detail);
+		String[] item13Detail = this.getResources().getStringArray(R.array.item13Detail);
 		//添加项目明细
 		rightList.addAll(Arrays.asList(item1Detail ));
 		rightList.addAll(Arrays.asList(item2Detail ));
@@ -151,6 +211,74 @@ public class ScoringActivity extends Activity {
 		rightList.addAll(Arrays.asList(item11Detail));
 		rightList.addAll(Arrays.asList(item12Detail));
 		rightList.addAll(Arrays.asList(item13Detail));
+	}
+	
+	/**
+	 * 左边栏的点击事件
+	 * @param id
+	 */
+	private void clickLeftListItem(int id){
+		Log.d(TAG, "clickLeftListItem(), id: "+ id);
+		rightList = new ArrayList<String>();
+		switch(id){
+			case R.string.item1: 
+				String[] item1Detail = this.getResources().getStringArray(R.array.item1Detail);
+				rightList.addAll(Arrays.asList(item1Detail ));
+				break; 
+			case R.string.item2: 
+				String[] item2Detail = this.getResources().getStringArray(R.array.item2Detail);
+				rightList.addAll(Arrays.asList(item2Detail ));
+				break; 
+			case R.string.item3: 
+				String[] item3Detail = this.getResources().getStringArray(R.array.item3Detail);
+				rightList.addAll(Arrays.asList(item3Detail ));
+				break; 
+			case R.string.item4: 
+				String[] item4Detail = this.getResources().getStringArray(R.array.item4Detail);
+				rightList.addAll(Arrays.asList(item4Detail ));
+				break; 
+			case R.string.item5: 
+				String[] item5Detail = this.getResources().getStringArray(R.array.item5Detail);
+				rightList.addAll(Arrays.asList(item5Detail ));
+				break; 
+			case R.string.item6: 
+				String[] item6Detail = this.getResources().getStringArray(R.array.item6Detail);
+				rightList.addAll(Arrays.asList(item6Detail ));
+				break; 
+			case R.string.item7: 
+				String[] item7Detail = this.getResources().getStringArray(R.array.item7Detail);
+				rightList.addAll(Arrays.asList(item7Detail ));
+				break; 
+			case R.string.item8: 
+				String[] item8Detail = this.getResources().getStringArray(R.array.item8Detail);
+				rightList.addAll(Arrays.asList(item8Detail ));
+				break; 
+			case R.string.item9: 
+				String[] item9Detail = this.getResources().getStringArray(R.array.item9Detail);
+				rightList.addAll(Arrays.asList(item9Detail ));
+				break; 
+			case R.string.item10: 
+				String[] item10Detail = this.getResources().getStringArray(R.array.item10Detail);
+				rightList.addAll(Arrays.asList(item10Detail));
+				break; 
+			case R.string.item11: 
+				String[] item11Detail = this.getResources().getStringArray(R.array.item11Detail);
+				rightList.addAll(Arrays.asList(item11Detail));
+				break; 
+			case R.string.item12: 
+				String[] item12Detail = this.getResources().getStringArray(R.array.item12Detail);
+				rightList.addAll(Arrays.asList(item12Detail));
+				break; 
+			case R.string.item13: 
+				String[] item13Detail = this.getResources().getStringArray(R.array.item13Detail);
+				rightList.addAll(Arrays.asList(item13Detail));
+				break; 
+		}
+		Log.d(TAG, "rightList: " + rightList);
+		adapterRight = new ListViewActivityAdapter(this, rightList);
+		rightListView.setAdapter(adapterRight);
+		//通知list内容已经改变
+		adapterRight.notifyDataSetChanged();
 	}
 
 }
